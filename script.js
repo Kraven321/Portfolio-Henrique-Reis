ca4cd3c3d10f4af38b333959fceef8b8b0dd99cd
Henrique  Reis <henriquereiscarvalho@outlook.com>
2023-05-09 13:05:56 -0300

    terceiro commit-portfolio

---
+++ script.js
@@ -0,0 +1,66 @@
+
+// Seleciona o elemento header
+const header = document.querySelector("header");
+
+// Adiciona um listener para o evento "scroll" da janela
+window.addEventListener("scroll", () => {
+  // Verifica a posição de rolagem da página
+  if (window.scrollY > 0) {
+    // Se a posição de rolagem for maior que zero, adiciona a classe "scrolled" ao elemento "header"
+    header.classList.add("scrolled");
+  } else {
+    // Caso contrário, remove a classe "scrolled"
+    header.classList.remove("scrolled");
+  }
+});
+
+
+// nessa linha chamamos o id que foi definido na tag <strong> no doc html
+const textDisplay = document.getElementById('efeito')
+// nessa linha são definidas as tres frases que serão animadas
+const phrases = ['Henrique Reis']
+let i = 0
+let j = 0 
+let currentPhrase = []
+let isDeleting = false
+let isEnd = false
+
+function loop () {
+  isEnd = false
+  textDisplay.innerHTML = currentPhrase.join('')
+
+  if (i < phrases.length) {
+
+    if (!isDeleting && j <= phrases[i].length) {
+      currentPhrase.push(phrases[i][j])
+      j++
+      textDisplay.innerHTML = currentPhrase.join('')
+    }
+
+    if(isDeleting && j <= phrases[i].length) {
+      currentPhrase.pop(phrases[i][j])
+      j--
+      textDisplay.innerHTML = currentPhrase.join('')
+    }
+
+    if (j == phrases[i].length) {
+      isEnd = true
+      isDeleting = true
+    }
+
+    if (isDeleting && j === 0) {
+      currentPhrase = []
+      isDeleting = false
+      i++
+      if (i === phrases.length) {
+        i = 0
+      }
+    }
+  }
+  const spedUp = Math.random() * (80 -50) + 50
+  const normalSpeed = Math.random() * (300 -200) + 200
+  const time = isEnd ? 2000 : isDeleting ? spedUp : normalSpeed
+  setTimeout(loop, time)
+}
+
+loop()
